f8b3ce911a29297c74bddfbb76e491b81ab999a9
Johnathan Chastain <chastainjohnathan75@gmail.com>
2025-02-04 19:37:06 -0600

    started writing tests

--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -1,5 +1,6 @@
-
+import { DocumentNode } from 'graphql';
+import { IResolvers } from '@graphql-tools/utils';
 import { typeDefs as requestTypeDefs, resolvers as requestResolvers } from './Request.js';
 
-export const typeDefs = [requestTypeDefs];
-export const resolvers = [requestResolvers];
+export const typeDefs: DocumentNode[] = [requestTypeDefs];
+export const resolvers: IResolvers[] = [requestResolvers];
