503a8acb20ef8937f6dd309697bd29c1568de8ea
JohnathanChastain <johnathanchastain435@gmail.com>
2025-02-09 14:33:17 -0600

    fixed mongoDB connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/localdb';
-
+// console.log('MONGO_URI:', MONGO_URI);
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
