d54a5b398143c696ff1acc724b7a86c51afcc828
Hannah Myers <hannahmyers510@gmail.com>
2025-02-09 17:10:40 -0500

    changed the user.ts

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to the Local Volunteer and Resource Finder</h1>
-      <p>Connecting volunteers and resources with those in need.</p>
+      <p>Connecting volunteers and resources with those in need in these dire times.</p>
     </div>
   );
 };
